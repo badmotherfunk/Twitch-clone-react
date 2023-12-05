6cf530fe6a43fa5879e6396542b3d7c032859191
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2023-12-05 16:35:19 +0100

    Amélioration de l'animation du coeur sur le bouton suivre de la section GameStreams

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -95,49 +95,48 @@ export default function Sidebar() {
   return (
     <div className={active ? 'sidebarActive' : 'sidebar'}>
 
-        <div className="containerTitre">
-          <h2 className={active ? 'titreSidebarActive' : 'titreSidebar'}>CHAÎNES RECOMMANDÉES</h2>
+      <div className="containerTitre">
+        <h2 className={active ? 'titreSidebarActive' : 'titreSidebar'}>CHAÎNES RECOMMANDÉES</h2>
           
-          {!active ? 
-          <button className='toggleSidebar'>
-            <svg onClick={toggleSidebar} width="20px" version="1.1" viewBox="0 0 20 20" x="0px" y="0px" aria-hidden="true" focusable="false" class="ScIconSVG-sc-1q25cff-1 jpczqG"><g><path d="M16 16V4h2v12h-2zM6 9l2.501-2.5-1.5-1.5-5 5 5 5 1.5-1.5-2.5-2.5h8V9H6z"></path></g></svg>
+        {!active ? 
+        <button className='toggleSidebar'>
+          <svg onClick={toggleSidebar} width="20px" version="1.1" viewBox="0 0 20 20" x="0px" y="0px" aria-hidden="true" focusable="false" class="ScIconSVG-sc-1q25cff-1 jpczqG"><g><path d="M16 16V4h2v12h-2zM6 9l2.501-2.5-1.5-1.5-5 5 5 5 1.5-1.5-2.5-2.5h8V9H6z"></path></g></svg>
+        </button>
+        : 
+        <div className='sidebarIcons'>
+          <button className='toggleSidebar' onClick={toggleSidebar}>
+            <svg width="20px" version="1.1" viewBox="0 0 20 20" x="0px" y="0px" aria-hidden="true" focusable="false" class="ScIconSVG-sc-1q25cff-1 jpczqG"><g><path d="M4 16V4H2v12h2zM13 15l-1.5-1.5L14 11H6V9h8l-2.5-2.5L13 5l5 5-5 5z"></path></g></svg>
           </button>
-          : 
-          <div className='sidebarIcons'>
-            <button className='toggleSidebar' onClick={toggleSidebar}>
-              <svg width="20px" version="1.1" viewBox="0 0 20 20" x="0px" y="0px" aria-hidden="true" focusable="false" class="ScIconSVG-sc-1q25cff-1 jpczqG"><g><path d="M4 16V4H2v12h2zM13 15l-1.5-1.5L14 11H6V9h8l-2.5-2.5L13 5l5 5-5 5z"></path></g></svg>
-            </button>
-              <svg width="20px" height="2rem" viewBox="0 0 20 20"><path fill="rgb(83 83 95)" fill-rule="evenodd" d="M12.002 3.999a2 2 0 0 1 2 2v2L18 6v8l-3.998-2v2a2 2 0 0 1-2 1.999h-8a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h8zM12 6H4v8h8V6z" clip-rule="evenodd"></path></svg>
-          </div>
-          }
-
+            <svg width="20px" height="2rem" viewBox="0 0 20 20"><path fill="rgb(83 83 95)" fill-rule="evenodd" d="M12.002 3.999a2 2 0 0 1 2 2v2L18 6v8l-3.998-2v2a2 2 0 0 1-2 1.999h-8a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h8zM12 6H4v8h8V6z" clip-rule="evenodd"></path></svg>
         </div>
-        <ul className="listeStream" >
+        }
 
-            {topStreams.map((stream, index) => (
-              <Link key={index} to={{pathname: `/live/${stream.user_login}`}}>
+      </div>
+      <ul className="listeStream" >
 
+        {topStreams.map((stream, index) => (
+          <Link key={index} to={{pathname: `/live/${stream.user_login}`}}>
 
-              <li className={active ? "containerFlexSidebarActive" : "containerFlexSidebar"}
-              aria-label= { `${stream.user_name} • ${stream.gameName} | ${stream.title} 🔴 Live | ${stream.viewer_count} spectateurs` }
-              >
+            <li className={active ? "containerFlexSidebarActive" : "containerFlexSidebar"}
+            aria-label= { `${stream.user_name} • ${stream.gameName} | ${stream.title} 🔴 Live | ${stream.viewer_count} spectateurs` }
+            >
 
-                <img src={stream.truePic} alt="logo user" className="profilePicRonde" />
+              <img src={stream.truePic} alt="logo user" className="profilePicRonde" />
               
-                <div className={active ? "streamUserActive" : "streamUser"}>{stream.user_name}</div>
+              <div className={active ? "streamUserActive" : "streamUser"}>{stream.user_name}</div>
 
-                <div className={active ? "viewerRightActive" : "viewerRight"}>
-                  <div className="redDot"></div>
-                  <div>{stream.viewer_count}</div>
-                </div>
+              <div className={active ? "viewerRightActive" : "viewerRight"}>
+                <div className="redDot"></div>
+                <div>{stream.viewer_count}</div>
+              </div>
 
-                <div className={active ? "gameNameSidebarActive" : "gameNameSidebar"}>{stream.gameName}</div>
+              <div className={active ? "gameNameSidebarActive" : "gameNameSidebar"}>{stream.gameName}</div>
 
-              </li>
-              </Link>
-            ))}
+            </li>
+          </Link>
+          ))}
             
-        </ul>
+      </ul>
 
     </div>
   )
