f170e746e21e6707af1cf4691739bc7e7d300d06
Raphael Lasplace <raphael.lasplace@yahoo.fr>
2023-12-26 18:39:21 +0100

    Ajout du texte alternatif et de la logique sur la bannière, quand l'utilisateur est sur la page live

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import Register from '../Register/Register'
 import './Banner.css'
 
 export default function Banner() {
 
+  //Récupère le pathname et extrait l'username du streamer
+  const location = useLocation()
+  const username = location.pathname.slice(6)
+
+  //Gère l'état de la modal pour le bouton s'inscrire
   const [isRegister, setIsRegister] = useState(false)
 
   const handleRegister = (e) => {
@@ -17,8 +23,17 @@ export default function Banner() {
       
       <div className="bannerContainer">
         <img alt="CoolCat" className="coolCat" src="https://static.twitchcdn.net/assets/coolcat-edacb6fbd813ce2f0272.png"/>
-        <h3 className='bannerTitre'>Rejoignez la communauté Twitch !</h3>
-        <h4 className='bannerTexte'>Découvrez les meilleurs streams où que vous soyez.</h4>
+        {username && location.pathname.match("/live/" + username) ?
+        <>
+          <h3 className='bannerTitre'>Inscrivez-vous pour profiter de ce que {username} a de mieux à offrir</h3>
+          <h4 className='bannerTexte'>Chattez, suivez ou abonnez-vous – Twitch vous appartient.</h4>
+        </>
+        :
+        <>
+          <h3 className='bannerTitre'>Rejoignez la communauté Twitch !</h3>
+          <h4 className='bannerTexte'>Découvrez les meilleurs streams où que vous soyez.</h4>
+        </>
+        }
       </div>
       <form>
         <button className='bannerButton' onClick={handleRegister}>
